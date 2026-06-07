c8b40ea6fea97f33eedd0b79d824a056db89f6c8
kustoro2026-oss <kustoro2026@gmail.com>
2026-06-07 19:37:33 +0700

    api vercel

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -1,8 +1,16 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
